bb05e2d487e4182243deae3f91f21e9050cb2d06
Adam Gariba <agariba101@gmail.com>
2021-07-05 13:25:13 -0400

    added app use method for setting headers

--- chat.js
+++ chat.js
@@ -5,14 +5,15 @@ const cors = require("cors");
 //         origins:["http://localhost:5000"]
 //     }
 // }
-corsOptions = {
-    cors:{
-        "origin": "*",
-        "methods": ["GET","POST"],
-        allowedHeaders: ["my-custom-header"],
-        credentials: true
-    }
-}
+// corsOptions = {
+//     cors:{
+//         "origin": "*",
+//         "methods": ["GET","POST"],
+//         allowedHeaders: ["my-custom-header"],
+//         credentials: true
+//     }
+// }
+corsOptions = {}
 const chalk = require('chalk'); // to style console.log texts
 const bodyParser = require("body-parser");
 const app = express();
@@ -21,6 +22,26 @@ const httpServer = require("http").createServer(app);
 const io = require("socket.io")(httpServer, corsOptions);
 app.use(cors());
 app.use(bodyParser.json());
+
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://happybiddingchat.herokuapp.com/');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 io.on("connection", socket => { 
     console.log("new client connected!")
